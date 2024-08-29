807a9fc61a156e7f481f77e03d05a0f7bc897f5a
amei <cms1984@msn.com>
2024-08-29 14:29:49 +0800

    nvim-config

--- lua/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('n', '<leader>cc', ':normal gcc<CR>', { desc = '[/] Toggle comment line' })
-return {
-		"folke/ts-comments.nvim",
-		opts = {},
-		event = "VeryLazy",
-		enabled = vim.fn.has("nvim-0.10.0") == 1,
-	}
